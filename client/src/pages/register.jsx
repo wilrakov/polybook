7eb664773e5b53e024b78c59708f78214aaf9398
Outrun <outrun.917@gmail.com>
2025-09-24 19:47:02 +0200

    refacto(client): register reformat

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -16,23 +16,33 @@ export default function Register() {
   });
 
   const handleSubmit = (event) => {
-
     mutation.mutate(new FormData(event.target));
-    
-    
   };
 
   return (
     <>
       <h1>Register</h1>
       <form onSubmit={handleSubmit}>
-        <input type="email" name="email" ref={emailRef} placeholder="johnDoe@exemple.com" />
-        <input type="text" name="password" ref={passwordRef} placeholder="password..." />
+        <input
+          type="email"
+          name="email"
+          ref={emailRef}
+          placeholder="johnDoe@exemple.com"
+        />
+        <input
+          type="text"
+          name="password"
+          ref={passwordRef}
+          placeholder="password..."
+        />
         <button type="submit">submit</button>
       </form>
 
-      { mutation.isPending ? "..." : (mutation.isSuccess ? mutation.data.message : mutation.error?.message)}
-
+      {mutation.isPending
+        ? "..."
+        : mutation.isSuccess
+        ? mutation.data.message
+        : mutation.error?.message}
     </>
   );
 }
