b6e72706af3c35bacfc1804c9d855b55a05e92aa
wilrakov <165658379+wilrakov@users.noreply.github.com>
2025-09-27 19:09:31 +0200

    refactor(server/app.py): Changement des lignes incorrectes
    Ok j'ai enlevé les appels faussés à db.py.
    Et j'ai retiré certaines lignes inutiles

--- server/app.py
+++ server/app.py
@@ -62,26 +62,20 @@ def check_email():
     if not email:
         return jsonify({"error": "Email requis"}), 400
 
-    # === RÉCUPÉRATION DE L'UTILISATEUR ===
-    # On cherche dans la DB si l'utilisateur existe
-    user = db.get_password_hash(email)
-    if user is None:
-        print("Aucun compte trouvé pour cet email")
-        return jsonify({"message": "Pas encore de compte"}), 404
-
     # === HASH DU MOT DE PASSE POUR LE LOGIN ===
     # On hash le mot de passe fourni avec le même salt que pour l'inscription
     t_sha = hashlib.sha512()
     t_sha.update(password.encode() + SALT_BYTES)
     hashed_password = base64.urlsafe_b64encode(t_sha.digest()).decode()
-
+    
     # === COMPARAISON AVEC LE HASH STOCKÉ ===
-    # On récupère le hash stocké dans la DB et on compare
-    stored_hash = user # adapter selon le nom de la colonne dans ta DB
-    if hashed_password == stored_hash:
+    # On vérifie si l'email et password correspondent
+    # db.authentificate_with_hash(email: str, hashed_password: str)
+    success = db.authenticate_with_hash(email, hashed_password)
+    if success:
         return jsonify({"token": token , "user":{"email" : email}}), 200
     else:
-        return jsonify({"error": "Mot de passe incorrect"}), 401
+        return jsonify({"error": "Mot de passe ou email incorrect"}), 401
 
 if __name__ == "__main__":
     app.run(debug=True)
